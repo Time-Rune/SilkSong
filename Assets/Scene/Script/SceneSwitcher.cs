0c1349413436d604eeef247a488623c93d9d00af
ztlltz <104620738+ztlltz@users.noreply.github.com>
2023-07-10 18:21:08 +0800

    scene

---
+++ Assets/Scene/Script/SceneSwitcher.cs
@@ -0,0 +1,28 @@
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour
+{
+
+    string s;
+    public void SwitchScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+    float stimes;
+
+    float ntimes;
+    private void Start()
+    {
+        stimes = Time.time;
+    }
+    private void Update()
+    {
+        ntimes = Time.time;
+        if (ntimes - stimes >= 3)
+        {
+            SwitchScene("MainMenu");
+        }
+    }
+}
